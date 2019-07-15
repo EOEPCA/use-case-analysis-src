305000e51e8f8800bb142546885c82ae72799b0f
Richard Conway <richard.conway@telespazio.com>
2019-07-15 16:09:57 +0100

    Offset local ports through which Use-Case-Analysis and Master-System-Design docs are served for local visualisation.

--- serve.sh
+++ serve.sh
@@ -4,11 +4,19 @@ ORIG_DIR="$(pwd)"
 cd "$(dirname "$0")"
 BIN_DIR="$(pwd)"
 
+DEFAULT_PORT="8000"
+
+PORT="$1"
+if [ -z "${PORT}" ]
+then
+  PORT="${DEFAULT_PORT}"
+fi
+
 BUILD_ROOT="build"
 OUTPUT_DIR="${BUILD_ROOT}/asciidoc/html5"
 
 cd "${OUTPUT_DIR}"
-xdg-open "http://localhost:8000" >/dev/null 2>&1
-python3 -m http.server
+xdg-open "http://localhost:${PORT}" >/dev/null 2>&1
+python3 -m http.server "${PORT}"
 
 cd "${ORIG_DIR}"
